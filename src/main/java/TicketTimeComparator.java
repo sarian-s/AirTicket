b87fd3e49b3fd9113d534e978c4b19e09b0b8703
sergey s <som-ana@yandex.ru>
2023-05-27 21:32:48 +0300

    initial commit

---
+++ src/main/java/TicketTimeComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket t1, Ticket t2) {
+        // ваш код
+    }
+}
